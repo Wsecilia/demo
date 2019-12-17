e4a798bc81bbe6fd0f1689ca05a355e5de170735
wsecilia <783819996@qq.com>
2019-12-18 00:59:40 +0800

    java demo

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String []args) {
+       System.out.println("Hello World!");
+	   System.out.println("Hello sonia!");
+	   System.out.println("Hello jack!");
+	   System.out.println("Hello amy!");
+    }
+}
